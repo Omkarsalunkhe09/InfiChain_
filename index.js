c35351277c1b7a2715e77d322f12570fa582e3ec
Omkarsalunkhe09 <ossalunkhe09@gmail.com>
2022-05-19 02:26:55 +0530

    upd

--- index.js
+++ index.js
@@ -20,16 +20,42 @@ ws.onopen = function () {
   sendData();
 };
 
+const sell_arr = [];
+const buy_arr = [];
+
+const avg = (elem) => {
+  const res =
+    elem.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue;
+    }, 0) / elem.length;
+
+  return res.toFixed(2);
+};
+
+const timer = setInterval(() => {
+  appendData(buy_arr, "buy");
+  appendData(sell_arr, "sell");
+}, 5000);
+
+const appendData = (payload, type) => {
+  if (payload) {
+    const result = avg(payload);
+    const Price = document.querySelector(`#${type}`);
+
+    Price.innerHTML = `${result}`;
+  }
+};
+
 ws.onmessage = (event) => {
   event = JSON.parse(event.data);
-  console.log(event.time);
   document.getElementById("time").innerHTML =
     event.time.substring(0, 10) + " " + event.time.substring(11, 19);
 
-  // if (event.side == "buy") {
-  //   document.getElementById("buy").innerHTML = event.price;
-  // }
+  if (event.side == "buy") {
+    buy_arr.push(+event.price);
+  }
 
-  // if (event.side == "sell")
-  //   document.getElementById("sell").innerHTML = event.price;
+  if (event.side == "sell") {
+    sell_arr.push(+event.price);
+  }
 };
